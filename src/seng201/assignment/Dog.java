9018e0b86ce5c87174fa84200f8dbc6c3672207a
Jacob McKenzie <jjm133@uclive.ac.nz>
2017-04-20 15:02:49 +1200

    updated dog

--- src/seng201/assignment/Dog.java
+++ src/seng201/assignment/Dog.java
@@ -4,8 +4,47 @@ package seng201.assignment;
  * Created by Matthew on 12/04/2017.
  */
 public class Dog extends Pet {
-    @Override
-    public String getSpecies() {
-        return null;
+    
+	private float[] weightRange = new float[] {20, 40};
+	private int[]  playfulnessRange = new int[] {3, 10};
+	private int[] roughnessRange = new int[] {5, 10};
+	private int[] hungerRateRange = new int[] {3, 4};
+	private int[] tiredRateRange = new int[] {2, 4};
+	
+	Dog(String name){
+		super(name);
+	}
+	
+	
+	@Override
+	protected String getSpecies() {
+        return "Dog";
     }
+	
+	protected Toy[] getFavouriteToy(){
+		return new Toy[] {Toy.LARGEBALL, Toy.SQUEAKYTOY, Toy.SMALLBALL};
+	}
+	
+	protected Food[] getFavouriteFood(){
+		return new Food[] {Food.TUNA, Food.STEAK};
+	}
+	
+	protected float[] getSpeciesWeightRange(){
+		return weightRange;
+	}
+	
+	protected int[] getSpeciesPlayfulnessRange(){
+		return playfulnessRange;
+	}
+	protected int[] getSpeciesRoughnessRange(){
+		return roughnessRange;
+	}
+	protected int[] getSpeciesHungerRateRange(){
+		return hungerRateRange;
+	}
+	protected int[] getSpeciesTiredRateRange(){
+		return tiredRateRange;
+	}
+	
+	
 }
