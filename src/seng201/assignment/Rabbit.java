b83cae6577fff4facaba5333271dedaa88f81f84
Jacob McKenzie <jjm133@uclive.ac.nz>
2017-04-20 15:04:55 +1200

    updated rabbit

--- src/seng201/assignment/Rabbit.java
+++ src/seng201/assignment/Rabbit.java
@@ -4,8 +4,45 @@ package seng201.assignment;
  * Created by Matthew on 12/04/2017.
  */
 public class Rabbit extends Pet {
-    @Override
-    public String getSpecies() {
-        return null;
+    
+	private float[] weightRange = new float[] {1.5f, 2.5f};
+	private int[] playfulnessRange = new int[] {2, 5};
+	private int[] roughnessRange = new int[] {3, 6};
+	private int[] hungerRateRange = new int[] {1, 3};
+	private int[] tiredRateRange = new int[] {1, 2};
+	
+	Rabbit(String name){
+		super(name);
+	}
+	
+	@Override
+	protected String getSpecies() {
+        return "Rabbit";
     }
+    
+	protected Toy[] getFavouriteToy(){
+		return new Toy[] {Toy.CARDBOARDBOX, Toy.GUINEAPIGWHEEL,Toy.SMALLBALL};
+	}
+	
+	protected Food[] getFavouriteFood(){
+		return new Food[] {Food.CARROT, Food.LETTUCE, Food.PEAS};
+	}
+
+	protected float[] getSpeciesWeightRange(){
+		return weightRange;
+	}
+	
+	protected int[] getSpeciesPlayfulnessRange(){
+		return playfulnessRange;
+	}
+	protected int[] getSpeciesRoughnessRange(){
+		return roughnessRange;
+	}
+	protected int[] getSpeciesHungerRateRange(){
+		return hungerRateRange;
+	}
+	protected int[] getSpeciesTiredRateRange(){
+		return tiredRateRange;
+	}
+	
 }
