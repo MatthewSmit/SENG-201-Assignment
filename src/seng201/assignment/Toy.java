46fde43f03908f30d3d90ddafc0328a443ee59c6
Jacob McKenzie <jjm133@uclive.ac.nz>
2017-04-20 15:01:35 +1200

    updated toy

--- src/seng201/assignment/Toy.java
+++ src/seng201/assignment/Toy.java
@@ -3,14 +3,47 @@ package seng201.assignment;
 /**
  * Created by Matthew on 12/04/2017.
  */
-public class Toy {
+public enum Toy {
+	
+	//price, durability = 1->10 (1 lowest, 10 highest)
+	SMALLBALL(3, 3),
+	LARGEBALL(5, 5),
+	SQUEAKYTOY(8, 5),
+	GUINEAPIGWHEEL(7,4),
+	JUNGLEGYM(8,4),
+	CARDBOARDBOX(1,2);
+	
     private int price;
     private int durability;
 
-    void degrade() {
+    
+    Toy(int price, int durability){
+    	this.price = price;
+    	this.durability = durability;
+    }
+    
+    public int getPrice(){
+    	return price;
+    }
+    
+    public int getDurability(){
+    	return durability;
+    }
+    
+    public void degrade(Pet pet) {
+    	durability = durability-pet.getRoughness();
+    	System.out.println(name().toLowerCase() + " has degraded to " + getDurability() + " durability.");
     }
 
-    boolean isBroken() {
+    public boolean isBroken() {
+    	if (durability <= 0){
+    		System.out.println(name()  + " is broken!");
+    	}
         return durability <= 0;
     }
+    
+    
+
+
+
 }
