04dc4b739455c0be1e4863b51fecdc90f9a7c2f7
Matthew Smit <programmdude@gmail.com>
2017-04-20 12:01:30 +1200

    Food class as enum

--- src/seng201/assignment/Food.java
+++ src/seng201/assignment/Food.java
@@ -3,9 +3,40 @@ package seng201.assignment;
 /**
  * Created by Matthew on 12/04/2017.
  */
-public class Food {
-    private int price;
-    private int nutrition;
-    private int mealSize;
-    private int tastiness;
+public enum Food {
+    Carrot(2, 1, 1, 1),
+    Tuna(6, 1, 1, 1),
+    Lettuce(2, 1, 1, 1),
+    Steak(10, 1, 1, 1),
+    Seeds(4, 1, 1, 1),
+    Bloodworm(8, 1, 1, 1),
+    Peas(2, 1, 1, 1);
+
+    private final int price;
+    private final int nutrition;
+    private final int mealSize;
+    private final int tastiness;
+    
+    Food(int price, int nutrition, int mealSize, int tastiness) {
+        this.price = price;
+        this.nutrition = nutrition;
+        this.mealSize = mealSize;
+        this.tastiness = tastiness;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getNutrition() {
+        return nutrition;
+    }
+
+    public int getMealSize() {
+        return mealSize;
+    }
+
+    public int getTastiness() {
+        return tastiness;
+    }
 }
