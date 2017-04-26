8053f8d1bd32e001d49f6b6f2d7648639757f0c5
Jacob McKenzie <jjm133@uclive.ac.nz>
2017-04-27 11:27:38 +1200

    fixed cat.java

--- src/seng201/assignment/Cat.java
+++ src/seng201/assignment/Cat.java
@@ -4,33 +4,37 @@ package seng201.assignment;
  * Created by Matthew on 12/04/2017.
  */
 public class Cat extends Pet {
-	private static float[] weightRange = new float[] {1.5f, 2.5f};
-	private static int[] playfulnessRange = new int[] {2, 5};
-	private static int[] roughnessRange = new int[] {3, 6};
-	private static int[] hungerRateRange = new int[] {1, 3};
-	private static int[] tiredRateRange = new int[] {1, 2};
+	
+	private float[] weightRange = new float[] {2.5f, 11};
+	private int[]  playfulnessRange = new int[] {3, 9};
+	private int[] roughnessRange = new int[] {3, 7};
+	private int[] hungerRateRange = new int[] {2, 4};
+	private int[] tiredRateRange = new int[] {2, 4};
 
-	public Cat(String name){
+	Cat(String name){
 		super(name);
 	}
 	
 	@Override
-	public String getSpecies() {
+	protected String getSpecies() {
         return "Cat";
     }
-    
+	
 	protected Toy[] getFavouriteToy(){
-		return new Toy[] {Toy.CARDBOARDBOX, Toy.GUINEAPIGWHEEL, Toy.SMALLBALL};
+		return new Toy[] {Toy.CARDBOARDBOX, Toy.SMALLBALL};
 	}
 	
 	protected Food[] getFavouriteFood(){
-		return new Food[] {Food.CARROT, Food.LETTUCE, Food.PEAS};
+		return new Food[] {Food.TUNA, Food.STEAK};
 	}
 
 	protected float[] getSpeciesWeightRange(){
 		return weightRange;
 	}
-	protected int[] getSpeciesPlayfulnessRange() { return playfulnessRange; }
+	
+	protected int[] getSpeciesPlayfulnessRange(){
+		return playfulnessRange;
+	}
 	protected int[] getSpeciesRoughnessRange(){
 		return roughnessRange;
 	}
