d0ce18900f5477ff5f555b4b7942f82bee962dda
Jacob McKenzie <jjm133@uclive.ac.nz>
2017-05-25 18:05:40 +1200

    a

--- src/seng201/assignment/SicknessEvent.java
+++ src/seng201/assignment/SicknessEvent.java
@@ -1,32 +1,81 @@
 package seng201.assignment;
 
-/**
- * The sickness event, which makes the pet sick. 
- * A greater chance of death the more hungry, tired and toilet-needy a pet is.
- */
-public final class SicknessEvent extends Event {
-	
-	/**
-	 * Processes a pet at the end of the turn to check if it becomes sick.
-	 * @param pet - the pet to be processed.
-	 * @return true - if pet becomes sick in the turn, false - if pet is already sick or does not become sick.
-	 **/
-    @Override
-    protected boolean processPet(final Pet pet) {
-        if (pet.isSick() || pet.isDead()) {
-            return false;
-        }
+import static org.junit.Assert.*;
 
-        float chance = lerp(0, 0.2f, (pet.getHunger() - 10) / 10.0f)
-                + lerp(0, 0.2f, (pet.getTiredness() - 10) / 10.0f)
-                + lerp(0, 0.2f, (pet.getToiletNeed() - 10) / 10.0f);
+import java.util.ArrayList;
 
-        if (getRandom() <= chance) {
-            pet.startBeingSick();
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-            return true;
-        }
+public final class PlayerTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    private Pet testCat = PetType.Cat.create("testCat");
+    private Pet testDog = PetType.Dog.create("testDog");
+    private Pet testBird = PetType.Bird.create("testBird");
+    private Pet[] petList = new Pet[] {testCat, testDog, testBird};
 
-        return false;
+    private Player testPlayer = new Player("test", petList);
+
+    @Test
+    public void testPlayer() {
+        assertTrue(testPlayer.getName().equals("test"));
+        assertTrue(testPlayer.getPets() == petList);
+    }
+
+    @Test
+    public void testPurchaseToy() {
+        assertTrue(testPlayer.getToys().size() == 0);
+
+        testPlayer.purchase(Toy.SMALLBALL);
+        assertTrue(testPlayer.getToys().get(0).toString().equals(Toy.SMALLBALL.toString()));
+        assertTrue(testPlayer.getToys().size() == 1);
+
+        testPlayer.purchase(Toy.LARGEBALL);
+        assertTrue(testPlayer.getToys().get(0).toString().equals(Toy.SMALLBALL.toString()));
+        assertTrue(testPlayer.getToys().get(1).toString().equals(Toy.LARGEBALL.toString()));
+        assertTrue(testPlayer.getToys().size() == 2);
+    }
+
+    @Test
+    public void testPurchaseFood() {
+        ArrayList<Food> foods = new ArrayList<>();
+        assertEquals(testPlayer.getFood(), new ArrayList<>());
+
+        foods.add(Food.CARROT);
+        testPlayer.purchase(Food.CARROT);
+        assertEquals(testPlayer.getFood(), foods);
+
+        foods.add(Food.LETTUCE);
+        testPlayer.purchase(Food.LETTUCE);
+        assertEquals(testPlayer.getFood(), foods);
+    }
+    
+    @Test
+    public void testPurchaseFailsWhenBroke() {
+        thrown.expect(UnsupportedOperationException.class);
+        testPlayer.addMoney(-testPlayer.getMoney());
+        testPlayer.purchase(Food.BLOODWORM);
+    }
+
+    @Test
+    public void testGetName() {
+        assertTrue(testPlayer.getName().equals("test"));
+    }
+
+    @Test
+    public void testGetPets() {
+        assertArrayEquals(petList, testPlayer.getPets());
+    }
+
+    @Test
+    public void testGetMoney() {
+        assertEquals(testPlayer.getMoney(), 100);
+        testPlayer.purchase(Toy.SMALLBALL);
+        assertEquals(testPlayer.getMoney(), 100 - Toy.SMALLBALL.getPrice());
+        testPlayer.purchase(Food.CARROT);
+        assertEquals(testPlayer.getMoney(), 100 - Toy.SMALLBALL.getPrice() - Food.CARROT.getPrice());
     }
 }
